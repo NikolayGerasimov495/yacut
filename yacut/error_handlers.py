0c192df959e2d6f637546630b4e58253429a026e
Nikolay Gerasimov <nikolay.gerasimov495@yandex.ru>
2024-10-12 18:07:45 +0300

    add view, models, form

--- yacut/error_handlers.py
+++ yacut/error_handlers.py
@@ -12,7 +12,6 @@ class InvalidAPIUsage(Exception):
         if status_code is not None:
             self.status_code = status_code
 
-
     def to_dict(self):
         return dict(message=self.message)
 
